a7c3bc608f4d0fa2899e70897ce8cc7a7d178dfd
ivantw829 <ivantw829@gmail.com>
2023-02-23 21:49:34 +0800

    EP-2

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot()
+#定義機器人(前綴與intent)
+bot = commands.Bot(command_prefix="$",intents=discord.Intents.all())
+
+@bot.command()
+async def here(ctx):
+    await ctx.send(F"{ctx.author}你好,你現在在{ctx.guild.name}的{ctx.channel.mention}")
 
 bot.run("")
